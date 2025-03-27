ac1d1b2052c5db8ac0e674c5fdb4259d295a4235
Viktor Khashchevyy <viktor@condense.tech>
2025-03-27 14:18:45 +0100

    secondaty source codebuild aft_account_customizations_terraform

--- modules/aft-customizations/codebuild.tf
+++ modules/aft-customizations/codebuild.tf
@@ -130,6 +130,21 @@ resource "aws_codebuild_project" "aft_account_customizations_terraform" {
     buildspec = data.local_file.aft_account_customizations_terraform.content
   }
 
+  secondary_sources {
+    type = "GITHUB"
+    source_identifier = "githubUrbanvAccountModules"
+    location = "https://github.com/urban-v/lz-account-modules"
+
+    auth {
+      type = "CODECONNECTIONS"
+      resource = "arn:aws:codestar-connections:eu-south-1:212178105454:connection/ce264050-f8e5-4b46-83ef-f3d154a23be2"
+    }
+  }
+  secondary_source_version {
+    source_identifier = "githubUrbanvGlobalModules"
+    source_version = "testing"
+  }
+
   vpc_config {
     vpc_id             = var.aft_vpc_id
     subnets            = var.aft_vpc_private_subnets
